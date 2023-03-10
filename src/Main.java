2da5838b1d2079c02f05a2e9065a0532971244fb
linards <linardssils1@gmail.com>
2023-03-10 15:46:15 +0200

    adding pizzas printing

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,14 @@
 import java.awt.*;
+import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
+        ArrayList<Pizza> pizzaPresets = PizzaPresets.makePresets();
+
+        for(Pizza pizza : pizzaPresets){
+            System.out.println(pizza.toString());
+        }
+
         new MainMenu();
     }
 }
