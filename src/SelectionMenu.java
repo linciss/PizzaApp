d5171c8117d4f0d99870a22e482a12cbe8da57db
linards <linardssils1@gmail.com>
2023-03-10 15:48:05 +0200

    adding scroll bar

--- src/SelectionMenu.java
+++ src/SelectionMenu.java
@@ -1,6 +1,24 @@
-public class SelectionMenu {
+import javax.swing.*;
 
+public class SelectionMenu extends JPanel {
 
+    MainMenu menu;
+
+    public SelectionMenu(MainMenu menu){
+    this.menu = menu;
+    initPanel();
+    }
+
+    public void initPanel(){
+
+        setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+
+        JScrollPane scrollBar=new JScrollPane(pizzaPanel, JScrollPane.VERTICAL_SCROLLBAR_ALWAYS, JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
+        scrollBar.getVerticalScrollBar().setUnitIncrement(16);
+        add(scrollBar);
+    }
+
+    JPanel pizzaPanel = new JPanel();
 
 
 
