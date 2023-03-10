1f351e402291326747bc5886aab282f4970c0427
linards <linardssils1@gmail.com>
2023-03-10 15:46:32 +0200

    adding selection menu

--- src/MainMenu.java
+++ src/MainMenu.java
@@ -1,12 +1,13 @@
 import javax.swing.*;
 
 public class MainMenu extends JFrame {
+    SelectionMenu selectionMenu = new SelectionMenu(this);
     public MainMenu (){
         setSize(400, 600);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        add(selectionMenu);
 
         setVisible(true);
-
 
     }
 
