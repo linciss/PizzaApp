7b1cd2168dd424b0fc6454e7c167b01e59a1c233
linards <linardssils1@gmail.com>
2023-03-11 19:31:15 +0200

    adding distance

--- src/Objects/Person.java
+++ src/Objects/Person.java
@@ -5,10 +5,19 @@ import javax.swing.*;
 public class Person {
 
     private String name, address;
+    private double distance;
 
-    public Person(String name, String address){
+    public Person(String name, String address, double distance){
         this.name = name;
         this.address = address;
+        this.distance = distance;
+    }
+
+    public double getDistance() {
+        return distance;
+    }
+    public void setDistance(double distance) {
+        this.distance = distance;
     }
     public String getName() {
         return name;
