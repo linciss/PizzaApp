5f9a32a91a65f015b5850c849b607db7a08e3234
linards <linardssils1@gmail.com>
2023-03-16 00:03:09 +0200

    Update

--- src/Menus/Window.java
+++ src/Menus/Window.java
@@ -2,19 +2,24 @@ package Menus;
 
 import Menus.LoginMenu;
 import Menus.SelectionMenu;
+import Objects.Log;
 import Objects.Person;
 import com.formdev.flatlaf.*;
 import com.formdev.flatlaf.themes.FlatMacLightLaf;
 
 import javax.swing.*;
 import java.awt.*;
+import java.util.ArrayList;
 
 public class Window extends JFrame {
+    Log log;
     PersonInfoMenu personInfoMenu;
     ConfirmationMenu confirmationMenu;
     SelectionMenu selectionMenu = new SelectionMenu(this, confirmationMenu);
     public Person person;
     LoginMenu loginMenu = new LoginMenu(this, selectionMenu);
+
+    ArrayList<Log> orderHistory = new ArrayList<>();
 
 
     //Frame constructor
